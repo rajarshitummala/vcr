c19aa502de46d5ae3d75c1f9a2017e6088d99178
@trajarshi <trajarshi@rlogin.cs.vt.edu>
2026-02-25 22:04:02 -0500

    handled celsius/farenheit correctly

--- app.js
+++ app.js
@@ -1,24 +1,47 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+let latitude = null;
+let longitude = null;
 let data = null;
 let scale = null;
 
+// Process the data from zipcode
+function setLocation(data) {
+  data = data.results.pop();
+  latitude = data.latitude;
+  longitude = data.longitude;
+}
+
+// Process the temperature data into a JSON object
 function setData(dat) {
+  console.log(dat);
   data = {"temperature": dat.current.temperature_2m, "scale": scale};
 }
 
 app.use(express.json());
 
 app.get('/locations/:zip', async (req, res) => {
-  scale = req.query.scale;
+  scale = req.query.scale || "Fahrenheit";
   const zip = req.params.zip;
-  const url = "https://api.open-meteo.com/v1/forecast?latitude=37.2296&longitude=-80.4139&current=temperature_2m";
+  const geocodeUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${zip}&count=1&language=en&format=json&countryCode=US`;
   
-  await fetch(url)
+  // Call API to convert zipcode to lat/long
+  await fetch(geocodeUrl)
+    .then( res => res.json() )
+    .then( data => setLocation(data));
+
+  // Set the scale correctly
+  let weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m&temperature_unit=fahrenheit`;
+  if (scale == "Celsius") {
+    weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m`;
+  }
+
+  // Call API to find temperature data of lat/long
+  await fetch(weatherUrl)
     .then( res => res.json() )
     .then( data => setData(data));
-  res.send(data);
+  res.status(200).send(data);
 });
 
 app.listen(port, () => {
