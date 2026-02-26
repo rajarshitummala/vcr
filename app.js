a1e8ad7beb8ae762b84d7b5a30485afd2bf2096a
@trajarshi <trajarshi@rlogin.cs.vt.edu>
2026-02-25 19:14:28 -0500

    setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('test');
+});
+
+app.listen(port, () => {
+  console.log(`${port}`);
+});
